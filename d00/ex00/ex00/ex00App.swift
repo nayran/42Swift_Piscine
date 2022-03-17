15ef22e73e9267a737162c8125b3ee487bd8d98f
Nayran Ziebell <nayran@Nayrans-MacBook-Pro.local>
2022-03-17 18:14:47 -0300

    d00ex00

---
+++ d00/ex00/ex00/ex00App.swift
@@ -0,0 +1,17 @@
+//
+//  ex00App.swift
+//  ex00
+//
+//  Created by Nayran Ziebell on 17/03/22.
+//
+
+import SwiftUI
+
+@main
+struct ex00App: App {
+    var body: some Scene {
+        WindowGroup {
+            buttonView()
+        }
+    }
+}
