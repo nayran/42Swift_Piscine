75eb102772031fda8af1f1ff748c5c60b4dc36ca
Nayran Ziebell <nayran@Nayrans-MacBook-Pro.local>
2022-03-17 19:28:51 -0300

    d00ex01

--- d00/ex00/ex00/buttonView.swift
+++ d00/ex00/ex00/buttonView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct buttonView: View {
     var body: some View {
---
+++ d00/ex01/ex01/ex01App.swift
@@ -0,0 +1,17 @@
+//
+//  ex01App.swift
+//  ex01
+//
+//  Created by Nayran Ziebell on 17/03/22.
+//
+
+import SwiftUI
+
+@main
+struct ex01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
